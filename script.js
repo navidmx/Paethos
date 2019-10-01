7b47114c9b5705cdf634efdca3d42f08c3cb8b1d
navidmx <navidmx@gmail.com>
2019-10-01 13:13:53 -0400

    Updated to forest env

---
+++ script.js
@@ -0,0 +1,13 @@
+let env = document.querySelector('#env');
+env.setAttribute('environment', {
+    preset: 'forest',
+    fog: 0.9,
+    skyType: 'gradient',
+    skyColor: '#76d6ff',
+    horizonColor: 'white',
+    playArea: 1,
+    ground: 'hills',
+    groundTexture: 'checkerboard',
+    groundYScale: 10,
+    dressingAmount: 500
+});
