5c5a4fca7f5f8674cf53eb90fe6f3e734391ba68
Gabriel Rasskin <43894452+grasskin@users.noreply.github.com>
2020-01-16 12:42:50 -0500

    for nav

--- public/js/transitions.js
+++ public/js/transitions.js
@@ -1,9 +1,3 @@
 function blackOut() {
-    overlay = document.createElement('div');
-    document.querySelector('body').appendChild(overlay);
-    setAttributes(overlay, {
-        position: 'absolute',
-        width: '100vw',
-        height: '100vh'
-    });
+    overlay = document.querySelector('#overlay');
 }
