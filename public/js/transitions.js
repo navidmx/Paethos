5e99a813e20217d0a796db825898c92196252eb4
Gabriel Rasskin <43894452+grasskin@users.noreply.github.com>
2020-01-16 12:21:52 -0500

    org

---
+++ public/js/transitions.js
@@ -0,0 +1,15 @@
+const setAttributes = (el, attrs) => {
+    for (let key in attrs) {
+        el.setAttribute(key, attrs[key]);
+    }
+};
+
+function blackOut() {
+    overlay = document.createElement('div');
+    document.querySelector('body').appendChild(overlay);
+    setAttributes(overlay, {
+        position: 'absolute',
+        width: '100vw',
+        height: '100vh'
+    });
+}
