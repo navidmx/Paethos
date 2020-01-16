ddd37f8625ee6702c23d39da9cadb1026e19ee7d
Gabriel Rasskin <43894452+grasskin@users.noreply.github.com>
2020-01-16 12:22:54 -0500

    merge issues

--- public/js/transitions.js
+++ public/js/transitions.js
@@ -1,9 +1,3 @@
-const setAttributes = (el, attrs) => {
-    for (let key in attrs) {
-        el.setAttribute(key, attrs[key]);
-    }
-};
-
 function blackOut() {
     overlay = document.createElement('div');
     document.querySelector('body').appendChild(overlay);
