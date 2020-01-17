8fc9651c827cd853e086eea7985ec0a76eb09a6d
Gabriel Rasskin <43894452+grasskin@users.noreply.github.com>
2020-01-17 01:33:18 -0500

    created gamma and updated beta

--- public/js/scenes.js
+++ public/js/scenes.js
@@ -4,14 +4,16 @@ let setEnvironment = (scene) => {
 };
 
 let sceneDelta = () => {
-    document.getElementsByClassName('environmentGround')[0]
+    document
+        .getElementsByClassName('environmentGround')[0]
         .setAttribute('animation', {
             property: 'position',
             to: '0 0 0',
             dur: 0,
             easing: 'easeInQuad'
         });
-    document.getElementsByClassName('environmentGround')[0]
+    document
+        .getElementsByClassName('environmentGround')[0]
         .setAttribute('animation__2', {
             property: 'scale',
             to: '1 1 1',
@@ -24,7 +26,7 @@ let sceneDelta = () => {
     setTitle(text.delta.title);
     setSubtitle(text.delta.subtitle);
     enableGUI('delta');
-}
+};
 
 let sceneTheta = () => {
     document
@@ -44,7 +46,7 @@ let sceneTheta = () => {
             easing: 'linear'
         });
     setEnvironment(scenes.theta);
-}
+};
 
 let sceneAlpha = () => {
     document
@@ -65,7 +67,7 @@ let sceneAlpha = () => {
         });
     setEnvironment(scenes.alpha);
     toggleOceanVisibility();
-}
+};
 
 let scenes = {
     initial: {
@@ -197,29 +199,54 @@ let scenes = {
     },
     beta: {
         active: true,
-        seed: 3,
+        seed: 52,
         skyType: 'gradient',
         skyColor: '#1fbfd1',
         horizonColor: '#ddd',
         lighting: 'point',
-        lightPosition: { x: -0.11, y: 0.16, z: 0.33 },
+        lightPosition: { x: -3.09, y: 3.36, z: 0.33 },
         fog: 0.67,
         flatShading: true,
         playArea: 1,
         ground: 'hills',
         groundYScale: 10,
         groundTexture: 'none',
-        groundColor: '#e7ce4f',
-        groundColor2: '#ffdc69',
+        groundColor: '#feb943',
+        groundColor2: '#b900ff',
         dressing: 'arches',
-        dressingAmount: 18,
+        dressingAmount: 25,
         dressingColor: '#795449',
-        dressingScale: 26,
-        dressingVariance: { x: 20, y: 40, z: 20 },
+        dressingScale: 30,
+        dressingVariance: { x: 15, y: 80, z: 15 },
         dressingUniformScale: true,
         dressingOnPlayArea: 0.04,
         grid: 'none',
         gridColor: '#ccc',
         shadow: false
+    },
+    gamma: {
+        skyType: 'gradient',
+        skyColor: '#0c009c',
+        horizonColor: '#e9ad8c',
+        lightPosition: '0.5 1 0',
+        fog: 0.8,
+        flatShading: true,
+        playArea: 3,
+        ground: 'canyon',
+        groundYScale: 9.76,
+        groundColor: '#C66344',
+        groundColor2: '#c96b4b',
+        groundTexture: 'squares',
+        dressingScale: 0.7,
+        dressingVariance: { x: 0.5, y: 40, z: 0.5 },
+        dressingUniformScale: false,
+        gridColor: '#239893',
+        shadow: true,
+        active: true,
+        seed: 20,
+        shadowSize: 10.75,
+        dressing: 'cylinders',
+        dressingAmount: 400,
+        dressingColor: '#c28a77'
     }
 };
